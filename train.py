2111a7ae3ca7523bb0572843bf847d94bdcbbd61
mehdii190 <mehdi190gym@gmail.com>
2023-11-03 12:52:15 +0330

    more

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+from torch.utils.data import random_split
 
 from datasets import load_dataset
 from tokenizers import Tokenizer
@@ -34,8 +34,17 @@ def get_or_build_tokenizer(config, ds, lang):
     return tokenizer
 
 
-
-
+def get_ds(config):
+    
+    ds_raw = load_dataset("opus_books", f"{config['lang_src']}-f{config['tgt_src']}", split="train")
+    
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config["lang_src"])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config["lang_tgt"])
+    
+    
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw , val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
 
 
 
